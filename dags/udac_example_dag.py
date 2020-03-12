68305ebfd7f4c49d23025bfc988d2f79c2f938c9
harini-akula <harini.akula01@gmail.com>
2020-03-12 06:54:04 +0000

    feat: Configure LoadDimensionOperator parameters

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -66,28 +66,44 @@ load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
     redshift_conn_id='redshift_conn',
-    insert_facts_query=songplay_table_insert,
+    insert_facts_query=SqlQueries.songplay_table_insert,
     facts_table_name='songplays'   
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift_conn',  
+    select_dim_query=SqlQueries.user_table_insert,
+    dim_table_name='public.users',
+    append_only_flag=False
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift_conn',  
+    select_dim_query=SqlQueries.song_table_insert,
+    dim_table_name='public.songs',
+    append_only_flag=False    
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift_conn',  
+    select_dim_query=SqlQueries.artist_table_insert,
+    dim_table_name='public.artists',
+    append_only_flag=False
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift_conn',  
+    select_dim_query=SqlQueries.time_table_insert,
+    dim_table_name='public.time',
+    append_only_flag=False
 )
 
 run_quality_checks = DataQualityOperator(
