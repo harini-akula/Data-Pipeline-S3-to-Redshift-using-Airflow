74101333c27c59499c0b5f9232dcb52f195dd14b
harini-akula <harini.akula01@gmail.com>
2020-03-12 21:02:11 +0000

    feat: Implement DataQualityOperator of the dag

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,23 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id='',
+                 table_rowcounts_tests={},
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table_rowcounts_tests = table_rowcounts_tests
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Execution of DataQualityOperator started...')
+        redshift = PostgresHook(self.redshift_conn_id)
+        
+        for table, exp_row_count in self.table_rowcounts_tests.items():
+            records = redshift.get_records(f'SELECT COUNT(*) FROM {table}')
+            actual_row_count = records[0][0]
+            if actual_row_count != exp_row_count:
+                raise ValueError(f'Data quality check failed. {table} returned {actual_row_count} records instead of {exp_row_count} records.')
+            self.log.info(f'Data quality on table {table} check passed with {actual_row_count} records.')   
+            
+        self.log.info('Data quality checks completed.')        
