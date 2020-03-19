cbcdb6ff324d6aa1e3340e7af67391f59ddad225
harini-akula <harini.akula01@gmail.com>
2020-03-19 23:54:05 +0000

    chore: Renamed tests.py to dq_tests.py

---
+++ plugins/helpers/dq_tests.py
@@ -0,0 +1,2 @@
+class Tests:
+    tests_results = {'public.staging_events':8056, 'public.staging_songs':29, 'public.songs':29, 'public.users':104, 'public.time':6820, 'public.artists':29}
--- plugins/helpers/tests.py
+++
@@ -1,2 +0,0 @@
-class Tests:
-    tests_results = {'public.staging_events':100, 'public.staging_songs':100, 'public.songplays':20}
