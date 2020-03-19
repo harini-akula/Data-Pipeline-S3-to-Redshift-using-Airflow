6a3b81af6ce9cc8673c59bb4265c43d436760077
harini-akula <harini.akula01@gmail.com>
2020-03-19 23:20:51 +0000

    feat: Merge drop table queries into create table

--- drop_tables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS public.staging_events;
-DROP TABLE IF EXISTS public.staging_songs;
-DROP TABLE IF EXISTS public.songplays;
-DROP TABLE IF EXISTS public.songs;
-DROP TABLE IF EXISTS public.artists;
-DROP TABLE IF EXISTS public.users;
-DROP TABLE IF EXISTS public.time;
