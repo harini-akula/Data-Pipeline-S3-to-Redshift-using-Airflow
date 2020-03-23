d12d8257be2dac932fa09dc55488bcf8f339c975
harini-akula <harini.akula01@gmail.com>
2020-03-23 01:43:41 +0000

    docs: Add comments to load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -3,7 +3,23 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
+    """
+    Description: This class represents an operator used to load data into
+    fact table.
 
+    Attributes:
+        ui_color: color of task created by instantiating this class on 
+            Airflow UI.
+        insert_query: SQL query to insert records into fact table.
+        redshift_conn_id: reference to a specific AWS redshift instance.  
+        insert_facts_query: SQL query to select records to be inserted 
+            into the fact table.
+        facts_table_name: fact table name
+        
+    Methods:
+        execute(self, context): inserts records into the given fact table
+            on the given Redshift instance. 
+    """
     ui_color = '#F98866'
     insert_query = """
     INSERT INTO {}
@@ -16,7 +32,6 @@ class LoadFactOperator(BaseOperator):
                  insert_facts_query="",
                  facts_table_name="",
                  *args, **kwargs):
-
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id=redshift_conn_id
         self.insert_facts_query=insert_facts_query
@@ -24,8 +39,13 @@ class LoadFactOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info('LoadFactOperator started.')
+        # Creating a postgres hook object
         redshift = PostgresHook(self.redshift_conn_id)
+        
+        # Formatting insert query
         formatted_insert_query=self.insert_query.format(self.facts_table_name, self.insert_facts_query)
+        
         self.log.info('Loading fact table started.')
+        # Running query to insert records into fact table on Redshift
         redshift.run(formatted_insert_query)        
         self.log.info('Records inserted into {} table.'.format(self.facts_table_name))
