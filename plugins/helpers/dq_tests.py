5e86ec7faea54e7f31d16e7089acbfb20e9f36a3
harini-akula <harini.akula01@gmail.com>
2020-03-23 01:28:07 +0000

    docs: Add comments to dq_tests.py

--- plugins/helpers/dq_tests.py
+++ plugins/helpers/dq_tests.py
@@ -1,2 +1,4 @@
 class Tests:
+    # Defining table names as dictionary keys and the number of records in a table as dictionary value
+    # Expected record count for a table post successful loading of data into the tables
     tests_results = {'public.staging_events':8056, 'public.staging_songs':29, 'public.songs':29, 'public.users':104, 'public.time':6820, 'public.artists':29}
